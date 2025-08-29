e73fa394e55b2201699b461fcb569739030ff43c
Victor Manuel Lopez Henao <v.lopez9@utp.edu.co>
2025-08-28 23:52:39 -0500

    Delete FBP2 directory
    delete

--- FBP2/FBP2_EJ5.py
+++
@@ -1,7 +0,0 @@
-codigos_empleados = ["ref-010", "ac-090", "ref-010", "xp-89", "as-65", "kj-11", "xp-89", "wx-23", "rk-87", "as-65"]
-
-codigo_empleados_sin_duplicados = list(dict.fromkeys(codigos_empleados))
-
-print (codigo_empleados_sin_duplicados)
-
-
--- FBP2/FBP2_EJ6.py
+++
@@ -1,8 +0,0 @@
-palabra = input("\nDigite una palabra: ")
-lista_consonantes = []
-
-for i in (palabra):
-    if i not in ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']:
-        lista_consonantes.append(i)
-    
-print (lista_consonantes)
